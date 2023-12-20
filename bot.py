c2ec4f845c3858573cb32e1ea582917ae42d7548
Даниил Орлов <danilorlov9898@yandex.ru>
2023-12-21 00:09:40 +0300

    Экмпериментировал с кнопками

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message, ReplyKeyboardRemove, ReplyKeyboardMarkup
+from aiogram.types import Message, ReplyKeyboardMarkup
 
 from config import BOT_TOKEN, location
-from keyboards import button_contacts, button_map
+from keyboards import button_contacts
 
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-keyboard = ReplyKeyboardMarkup(keyboard=[[button_contacts, button_map]],
+keyboard = ReplyKeyboardMarkup(keyboard=[[button_contacts]],
                                resize_keyboard=True)
 
 
@@ -27,24 +27,16 @@ async def process_start_command(message: Message):
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
-                         'я пришлю тебе твое сообщение'
-    )
+                         'я пришлю тебе твое сообщение')
 
 
 @dp.message(F.text == 'Контакты')
 async def command_contacts(message: Message):
-    await message.answer(
-        text='Телефон:  +7 921 565-16-15'
-    )
-
-
-@dp.message(F.text == 'Мы на карте')
-async def command_map(message: Message):
     await message.answer_location(
         location.latitude,
         location.longitude
     )
-    await message.answer('Московский пр-т., 159')
+    await message.answer('Московский пр-т., 159 \nТелефон:  +7 921 565-16-15')
 
 
 if __name__ == '__main__':
--- keyboards.py
+++ keyboards.py
@@ -2,4 +2,3 @@ from aiogram.types import KeyboardButton
 
 
 button_contacts = KeyboardButton(text='Контакты')
-button_map = KeyboardButton(text='Мы на карте')
