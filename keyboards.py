31a114f5d3a1e727576fdcb2688944007a61d0fe
Даниил Орлов <danilorlov9898@yandex.ru>
2023-12-20 22:54:42 +0300

    Загрузил код на github

---
+++ keyboards.py
@@ -0,0 +1,8 @@
+from aiogram.types import ReplyKeyboarButton, KeyboardButton, ReplyKeyboardRemove
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+kb_builder = ReplyKeyboardBuilder()
+
+
+button_1 = KeyboardButton(text='Кнопка 1')
+button_2 = KeyboardButton(text='Кнопка 2')
