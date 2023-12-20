8b0e56b96859cb83c08081138e48dc94a0f31d55
Даниил Орлов <danilorlov9898@yandex.ru>
2023-12-20 23:13:14 +0300

    Добавил keyboards.py для хранения клавиатур

--- keyboards.py
+++ keyboards.py
@@ -1,8 +1,6 @@
 from aiogram.types import ReplyKeyboarButton, KeyboardButton, ReplyKeyboardRemove
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-kb_builder = ReplyKeyboardBuilder()
 
-
-button_1 = KeyboardButton(text='Кнопка 1')
-button_2 = KeyboardButton(text='Кнопка 2')
+button_contacts = KeyboardButton(text='Контакты')
+button_map = KeyboardButton(text='Мы на карте')
