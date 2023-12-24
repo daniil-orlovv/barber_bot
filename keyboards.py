8bf8dc37bfecbad7041b1d83f617604bb529e877
Даниил Орлов <danilorlov9898@yandex.ru>
2023-12-24 21:59:26 +0300

    Добавил генератор клавиатур

--- keyboards.py
+++ keyboards.py
@@ -1,5 +1,42 @@
-from aiogram.types import KeyboardButton
+from aiogram.types import (KeyboardButton, InlineKeyboardButton,
+                           InlineKeyboardMarkup)
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+LEXICON: dict[str, str] = {
+    'btn_1': 'Кнопка 1'}
 
 
 button_contacts = KeyboardButton(text='Контакты')
 button_sign_up = KeyboardButton(text='Записаться')
+
+url_button = InlineKeyboardButton(
+    text='Записаться на сеанс',
+    url='https://n793568.yclients.com/company/304080/menu?o=m2024364'
+)
+
+
+def create_inline_kb(width: int,
+                     *args: str,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=LEXICON[button] if button in LEXICON else button,
+                callback_data=button))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button))
+
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=width)
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
