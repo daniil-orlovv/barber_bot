c84a0cbeb959648cd7734bb11084112a4ad4671e
Даниил Орлов <danilorlov9898@yandex.ru>
2024-01-29 21:58:33 +0300

    Добавил BASE_URL в config

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ from aiogram import types
 
 location = types.Location(latitude=59.869865, longitude=30.319785)
 
-BOT_TOKEN = '6691199047:AAHOr-ueqvD9Qn7WsVEx_YspHknm_6i5UXU'
-
 TIME = 'Время работы:  10:00 - 22:00'
 PHONE = 'Телефон:  +7 921 565-16-15'
 ADDRESS = 'Адрес:  Московский пр-т., 159'
+BASE_URL = 'https://api.yclients.com/api/v1'
