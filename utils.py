f47960b6f0afb3b78dc2c1364d8e2ca812badbbf
Даниил Орлов <danilorlov9898@yandex.ru>
2024-02-12 21:49:25 +0300

    Перенес функцию return_month в utils.py

--- utils.py
+++ utils.py
@@ -8,3 +8,12 @@ def check_date(date_str: str) -> bool:
     if month in free_days and day in free_days[month]:
         return True
     return False
+
+
+def return_month(value):
+    months_key = {
+        '1': 'Января', '2': 'Февраля', '3': 'Марта', '4': 'Апреля', '5': 'Мая',
+        '6': 'Июня', '7': 'Июля', '8': 'Августа', '9': 'Сентября',
+        '10': 'Октября', '11': 'Ноября', '12': 'Декабря'
+    }
+    return months_key.get(value)
