25ff8f68c5f226ed2ba0f5b56182df66c14d2d36
Даниил Орлов <danilorlov9898@yandex.ru>
2024-02-12 21:48:43 +0300

    Удалил неиспользуемый код

--- keyboards.py
+++ keyboards.py
@@ -3,42 +3,14 @@ import datetime
 from aiogram.types import (KeyboardButton, InlineKeyboardButton)
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
-LEXICON: dict[str, str] = {
-    'btn_1': 'Кнопка 1'}
+from utils import return_month
 
 
 current_year = datetime.datetime.now().year
 
 button_contacts = KeyboardButton(text='Контакты')
 button_sign_up = KeyboardButton(text='Записаться')
-
-times = (
-    '10:00', '11:00', '15:00'
-)
-
-url_button = InlineKeyboardButton(
-    text='Записаться на сеанс',
-    url='https://n793568.yclients.com/company/304080/menu?o=m2024364'
-)
-
-
-def create_keyboards(width, *args):
-    kb_builder = ReplyKeyboardBuilder()
-    buttons = []
-    for i in args:
-        buttons.append(KeyboardButton(text=i))
-
-    kb_builder.row(*buttons, width=width)
-    return kb_builder.as_markup(resize_keyboard=True)
-
-
-def return_month(value):
-    months_key = {
-        '1': 'Января', '2': 'Февраля', '3': 'Марта', '4': 'Апреля', '5': 'Мая',
-        '6': 'Июня', '7': 'Июля', '8': 'Августа', '9': 'Сентября',
-        '10': 'Октября', '11': 'Ноября', '12': 'Декабря'
-    }
-    return months_key.get(value)
+button_cancel = KeyboardButton(text='Отменить запись')
 
 
 def create_inline_kb(adjust, type, *args, **kwargs):
