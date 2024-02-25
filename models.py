22c37f6fb3d455b928ec4ed2da17641d2bf9013d
Даниил Орлов <danilorlov9898@yandex.ru>
2024-02-25 20:35:13 +0300

    Добавил модели для postgres с помощью sqlalchemy

---
+++ models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, Date, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+from bot import engine
+
+
+Base = declarative_base()
+
+
+class DataForRecord(Base):
+    __tablename__ = 'records'
+    id = Column(Integer, primary_key=True)
+    staff = Column(String(100), nullable=False)
+    service = Column(String(100), nullable=False)
+    date = Column(String())
+    time = Column(String())
+    name = Column(String(), nullable=False)
+    phone = Column(String())
+    email = Column(String(100), nullable=False)
+    comment = Column(String(100), nullable=True)
+
+
+Base.metadata.create_all(engine)
