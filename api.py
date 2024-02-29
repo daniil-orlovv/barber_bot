84a4cf4b9d5c5b4d3b04ecda47e4bf9ee684f132
Даниил Орлов <danilorlov9898@yandex.ru>
2024-02-29 18:36:29 +0300

    Исправил логику запросов для работы со всеми мастерами Поправил ошибку, при которой запрос на создание записи

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import datetime
+import json
 
 from dotenv import load_dotenv
 from config import BASE_URL
@@ -12,7 +13,6 @@ USER_TOKEN = os.getenv('USER_TOKEN', default='user_token')
 
 current_year = datetime.datetime.now().year
 company_id = 1004927
-staff_id = 2933362
 url = 'https://api.yclients.com/api/v1/book_record/977067/'
 headers = {
     'Authorization': f'Bearer {PARTNER_TOKEN}, User {USER_TOKEN}',
@@ -60,16 +60,13 @@ def get_free_staff():
     if 'data' in response_json:
         data = response_json['data']
         for item in data:
-            staffs[item.get('id')] = item.get('name')
+            staffs[item.get('name')] = str(item.get('id'))
 
-
+    print(staffs)
     return staffs
 
-def get_one_staff(name):
 
-
-
-def get_free_date():
+def get_free_date(staff_id):
     url = f'{BASE_URL}/book_dates/{company_id}?staff_id={staff_id}'
     response = requests.get(url, headers=headers)
     response_json = response.json()
@@ -85,7 +82,7 @@ def get_free_date():
         return quoted_dict
 
 
-def get_free_time(date):
+def get_free_time(staff_id, date):
     url = f'{BASE_URL}/book_times/{company_id}/{staff_id}/{date}?'
     response = requests.get(url, headers=headers)
     response_json = response.json()
@@ -98,43 +95,57 @@ def get_free_time(date):
     return free_times
 
 
-def get_free_services():
+def get_free_services(staff_id):
     url = f'{BASE_URL}/book_services/{company_id}?staff_id={staff_id}'
     response = requests.get(url, headers=headers)
     response_json = response.json()
     if 'data' in response_json:
         data = response_json['data']
-        free_services = []
+        free_services = {}
         services = data.get('services')
-        for service in services:
-            free_services.append(service.get('title'))
+        for item in services:
+            free_services[item.get('title')] = str(item.get('id'))
     return free_services
 
 
 async def create_session_api(data):
 
-    url = 'https://api.yclients.com/api/v1/book_record/{company_id}/'
+    time = data['time']
+    month, day = data['date'].split('-')
+    current_year = datetime.datetime.now().year
+    date_iso8601 = f'{current_year}-{int(month):02}-{int(day):02}T{time}:00+0300'
+
+    url = 'https://api.yclients.com/api/v1/book_record/1004927/'
+    print(f'Имя: {data["name"]}'
+          f'Коммент: {data["comment"]}')
     data_for_request = {
         "phone": data['phone'],
         "fullname": data['name'],
         "email": data['email'],
-        "code": "38829",
+        # "code": "38829",
         "comment": data['comment'],
         "type": "mobile",
-        "notify_by_sms": 6,
+        "notify_by_sms": 2,
         "notify_by_email": 24,
-        "api_id": "777",
+        # "api_id": "777",  # Необязательный параметр
         "appointments": [
             {
                 "id": 1,
                 "services": [
-                    14531077
+                    int(data['service'])
                 ],
-                "staff_id": 2933362,
-                "datetime": f'{current_year}-{data["date"]}T{data["time"]}:00.000Z',
+                "staff_id": int(data['staff']),
+                "datetime": date_iso8601,
             }
         ]
         }
 
+    json_data = json.dumps(data_for_request, ensure_ascii=False, indent=4)
     response = requests.post(url, headers=headers, json=data_for_request)
-    print(response.text)
+
+    print(f'Формат json: {json_data}')
+    print(f'Данные для создания записи через api: {data_for_request}')
+    print(f'Ответ API: {response.text}')
+
+
+print(get_free_services('3113480'))
