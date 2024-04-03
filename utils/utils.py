fedb88ca9c63142af278899d830d28edb1869d75
Даниил Орлов <danilorlov9898@yandex.ru>
2024-04-03 22:09:46 +0300

    Удалил ставшие ненужными утилиты для проверки колбэков(переместил проверки в фильтры -> filters/filters.py)

---
+++ utils/utils.py
@@ -0,0 +1,38 @@
+import datetime
+
+from models.models import DataForRecord
+
+
+current_year = datetime.datetime.now().year
+
+
+def return_month(value):
+    months_key = {
+        '1': 'Январь', '2': 'Февраль', '3': 'Март', '4': 'Апрель', '5': 'Май',
+        '6': 'Июнь', '7': 'Июль', '8': 'Август', '9': 'Сентябрь',
+        '10': 'Октябрь', '11': 'Ноябрь', '12': 'Декабрь'
+    }
+    return months_key.get(value)
+
+
+def create_object_for_db(kwargs):
+
+    object_db = DataForRecord(
+        staff=kwargs['staff_name'],
+        service=kwargs['service_title'],
+        date=kwargs['date'],
+        time=kwargs['time'],
+        name=kwargs['name'],
+        phone=kwargs['phone'],
+        email=kwargs['email'],
+        comment=kwargs['comment']
+    )
+    return object_db
+
+
+def to_normalize_date(date):
+
+    invert_date = date.split('-')[::-1]
+    normal_date = (
+        f'{invert_date[0]} {return_month(invert_date[1])} {current_year}')
+    return normal_date
