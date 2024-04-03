78fba3f5b09bc2207dcd2582a125dd0e7bc98f43
Даниил Орлов <danilorlov9898@yandex.ru>
2024-04-04 02:18:42 +0300

    Создал модуль для фильтров хэндлеров

---
+++ filters/filters.py
@@ -0,0 +1,56 @@
+from aiogram.types import CallbackQuery
+from aiogram.filters import BaseFilter
+from aiogram.fsm.context import FSMContext
+
+from external_services.yclients import (get_free_staff, get_free_services,
+                                        get_free_date, get_free_time)
+
+
+class CheckFreeStaff(BaseFilter):
+
+    async def __call__(self, callback: CallbackQuery) -> bool:
+        return callback.data in get_free_staff().values()
+
+
+class CheckFreeService(BaseFilter):
+
+    async def __call__(
+            self,
+            callback: CallbackQuery,
+            state: FSMContext
+    ) -> bool:
+
+        state_data = await state.get_data()
+        return callback.data in get_free_services(
+            state_data['staff_id']).values()
+
+
+class CheckFreeDate(BaseFilter):
+
+    async def __call__(
+            self,
+            callback: CallbackQuery,
+            state: FSMContext
+    ) -> bool:
+
+        state_data = await state.get_data()
+        staff_id = state_data['staff_id']
+        month, day = callback.data.split('-')
+        free_days = get_free_date(staff_id)
+        if month in free_days and day in free_days[month]:
+            return True
+        return False
+
+
+class CheckFreeTime(BaseFilter):
+
+    async def __call__(
+            self,
+            callback: CallbackQuery,
+            state: FSMContext
+    ) -> bool:
+
+        state_data = await state.get_data()
+        date = state_data['date']
+        staff_id = state_data['staff_id']
+        return callback.data in get_free_time(staff_id, date)
